4c0bd308bbb0470a2e4f49e0f024712734b1730a
Narotam-Mishra <narotam21jan@gmail.com>
2025-06-23 00:12:06 +0530

    flask-learning_added-form_p1

--- flask-first-app/app.py
+++ flask-first-app/app.py
@@ -1,32 +1,19 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    my_list = [10,20,30,40]
-    return render_template('index.html', mylist=my_list)
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route('/other')
-def other():
-    some_text = 'Hello flask from other'
-    return render_template('other.html', some_text = some_text)
-
-@app.route('/redirect_endpoint')
-def redirect_endpoint():
-    return redirect(url_for('other'))
-
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
-
-@app.template_filter('repeat')
-def repeat(s, times=2):
-    return s * times
-
-@app.template_filter('alternate_case')
-def alternate_case(s):
-    return ''.join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+        if username == 'flask' and password == 'password':
+            return 'Success'
+        else:
+            return 'Failure'
 
 if __name__ == '__main__':
     app.run(port=5576, debug=True)
