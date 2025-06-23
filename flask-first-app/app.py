6f8b5968ce1b1f193525e5058dbf1939b5b4a3cd
Narotam-Mishra <narotam21jan@gmail.com>
2025-06-24 00:40:47 +0530

    flask-learning_file_uploads

--- flask-first-app/app.py
+++ flask-first-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, template_folder='templates')
 
@@ -14,6 +14,23 @@ def index():
             return 'Success'
         else:
             return 'Failure'
+
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    
+@app.route('/handle_post', methods=['post'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}')
+
+    return jsonify({'message': 'Successfully written!'})   
 
 if __name__ == '__main__':
     app.run(port=5576, debug=True)
